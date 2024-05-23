20f3b73d589cfab95da609899621d4be7d1d76f6
ChadM95 <ChadM95@users.noreply.github.com>
2024-05-23 17:04:52 +0100

    add Entity Framework // link projects // begin outlining database classes

---
+++ CA3_RentalApp/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CA3_RentalApp
+{
+    internal class Booking
+    {
+        public int BookingId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+    }
+}
---
+++ CA3_RentalApp/RentalBookingData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace CA3_RentalApp
+{
+    internal class RentalBookingData : DbContext
+    {
+
+    }
+}
