e41061186d4eba3ddb6ff1ae088436e0b7fc153d
ChadM95 <ChadM95@users.noreply.github.com>
2024-05-24 14:57:27 +0100

    wire up item display when selected

--- CA3_RentalApp/MainWindow.xaml.cs
+++ CA3_RentalApp/MainWindow.xaml.cs
@@ -32,38 +32,76 @@ namespace CA3_RentalApp
 
             //get all records from Surfboards table
             var query = db.Surfboards.ToList();
-            
+
             //update surfboards datagrid to show all items
             dgSurfboards.ItemsSource = query;
 
-            //set combobox items
-            cbx1.ItemsSource = new string[] {"All","Shortboard", "Longboard", "Bodyboard" };
+            //set combobox items and default selection
+            cbx1.ItemsSource = new string[] { "All", "Shortboard", "Longboard", "Bodyboard" };
+            cbx1.SelectedItem = "All";
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-            //determine what was selected
-            string typeSelected = cbx1.SelectedValue.ToString();
-            DateTime startDate = (DateTime)StartDatePicker.SelectedDate;
-            DateTime endDate = (DateTime)EndDatePicker.SelectedDate;
-
-            //database connection
-            RentalBookingData db = new RentalBookingData();
-
-            //query database and display results in listbox depending on search criteria
-            if (typeSelected == "All")
-            { 
-                lbx1.ItemsSource = db.Surfboards.Where
-                (s => s.Bookings.All(b => b.StartDate >= endDate || b.EndDate <= startDate))
-                .ToList();
-            }
-
-            else
+            if (StartDatePicker.SelectedDate != null && EndDatePicker.SelectedDate != null)
             {
-                lbx1.ItemsSource = db.Surfboards.Where
-                (s => s.Bookings.All(b => b.StartDate >= endDate || b.EndDate <= startDate) && s.Type == typeSelected)
-                .ToList();
+                //determine what was selected
+                string typeSelected = cbx1.SelectedValue.ToString();
+                DateTime startDate = (DateTime)StartDatePicker.SelectedDate;
+                DateTime endDate = (DateTime)EndDatePicker.SelectedDate;
+
+                
+                    //database connection
+                    RentalBookingData db = new RentalBookingData();
+
+                    //query database and display results in listbox depending on search criteria
+                    if (typeSelected == "All")
+                    {
+                        lbx1.ItemsSource = db.Surfboards.Where
+                        (s => s.Bookings.All(b => b.StartDate >= endDate || b.EndDate <= startDate))
+                        .ToList();
+                    }
+
+                    else
+                    {
+                        lbx1.ItemsSource = db.Surfboards.Where
+                        (s => s.Bookings.All(b => b.StartDate >= endDate || b.EndDate <= startDate) && s.Type == typeSelected)
+                        .ToList();
+                    }
             }
+            //control if a value is not entered
+            else
+                MessageBox.Show("Please enter a value for each option");    
+        }
+
+            private void lbx1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+            {
+                //determine what was selected
+                var selectedItem = lbx1.SelectedItem as Surfboard;
+                DateTime startDate = (DateTime)StartDatePicker.SelectedDate;
+                DateTime endDate = (DateTime)EndDatePicker.SelectedDate;
+
+                //display text information
+                tblk1.Text = string.Format("<b>Selected Item</b>\nType: {0}\nStart Date: {1}\nEnd Date: {2}",
+                            selectedItem.Type.ToString(), startDate.ToShortDateString(), endDate.ToShortDateString());
+
+                //display picture
+                switch (selectedItem.Type)
+                {
+                    case "Shortboard":
+                        img1.Source = new BitmapImage(new Uri("pack://application:,,,/Content/shortboard.png"));
+                        break;
+                    case "Longboard":
+                        img1.Source = new BitmapImage(new Uri("pack://application:,,,/Content/longboard.png"));
+                        break;
+                    case "Bodyboard":
+                        img1.Source = new BitmapImage(new Uri("pack://application:,,,/Content/bodyboard.png"));
+                        break;
+                }
+
+            //make book button visible
+            btnBook.Visibility = Visibility.Visible;
+            }
+
         }
     }
-}
